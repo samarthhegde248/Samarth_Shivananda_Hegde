3565612031b9c119d72ab31aa29894ab1c6a3802
samarthhegde248 <samarth.hegde248@gmail.com>
2021-12-30 01:17:45 +0530

    added dockerfile and docker-compose file configuaration

---
+++ 9-CampaignManagementSystem/cms/database_image/capstone.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS capstone;
+CREATE DATABASE capstone;
+USE capstone;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS form_data;
+DROP TABLE IF EXISTS user_response;
+
+CREATE TABLE user (
+  firstname varchar(255) NOT NULL,
+  lastname varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  role varchar(255) NOT NULL,
+  PRIMARY KEY (email));
+
+CREATE TABLE form_data(
+	ID int(11) NOT NULL AUTO_INCREMENT,
+	form_title varchar(255) NOT NULL,
+	created_date datetime NOT NULL,
+	end_date datetime NULL,
+	available boolean NOT NULL default false,
+	form_format json NOT NULL,
+	list_of_mails varchar(255) NULL,
+	PRIMARY KEY (ID));
+
+CREATE TABLE user_response(
+	id int(11) NOT NULL AUTO_INCREMENT,
+	email varchar(255) NOT NULL,
+	survey_title varchar(255) NOT NULL,
+	submit_date datetime NOT NULL,
+	question0 varchar(255) NULL default NULL,
+	answer0 varchar(255) NULL default NULL,
+	question1 varchar(255) NULL default NULL,
+	answer1 varchar(255) NULL default NULL,
+	question2 varchar(255) NULL default NULL,
+	answer2 varchar(255) NULL default NULL,
+	question3 varchar(255) NULL default NULL,
+	answer3 varchar(255) NULL default NULL,
+	question4 varchar(255) NULL default NULL,
+	answer4 varchar(255) NULL default NULL,
+	question5 varchar(255) NULL default NULL,
+	answer5 varchar(255) NULL default NULL,
+	PRIMARY KEY (id));
+
+
+INSERT INTO user(firstname, lastname, email, password, role)VALUES('Roger', 'Kutcher', 'roger@gmail.com', 'admin@123', 'admin');
+INSERT INTO user(firstname, lastname, email, password, role)VALUES('Steve', 'Martin', 'steve@gmail.com', 'admin@123', 'admin');
+INSERT INTO user(firstname, lastname, email, password, role)VALUES('Samarth', 'Hegde', 'insamzone@gmail.com', 'user@123', 'user');
+
+select * from user;
